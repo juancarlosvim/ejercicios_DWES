38b0fae1052a8ce75ee011851d70a8283dfc933a
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2017-10-04 20:40:28 +0200

    Contar Carcteres
    Ejemplo Hola mundo, "o " deberá imprimir el número de veces de o que serían 2

---
+++ unidad 03/VB/ud03_tarea00/Default.aspx.vb
@@ -0,0 +1,53 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Private Sub _Default_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Dim frase As String = "Hola mundo"
+
+        Response.Write(pasarFrase("Esto es una prueba"))
+        Response.Write("<br/>")
+        Response.Write(pasarFrase("Esto es un ejemplo", "o"))
+    End Sub
+
+    Private Function pasarFrase(ByRef frase As String, Optional ByVal vocal As Char = Nothing) As Object
+        Dim encontrado As Object
+        Response.Write("La frase es => " & frase)
+        Response.Write("<br/>")
+        Response.Write("La vocal es => " & vocal)
+        Response.Write("<br/>")
+        Response.Write("La [  " & vocal & " ] se repite => " & encontrado)
+
+        If vocal = Nothing Then
+            encontrado = False
+            For i = 0 To frase.Length - 1
+
+                For j = i + 1 To frase.Length - 1
+
+                    If frase(i) = frase(j) Then
+                        encontrado = True
+                        GoTo salir
+                    End If
+                Next
+            Next
+
+            Return encontrado
+        Else
+            encontrado = 0
+
+            For i = 0 To frase.Length - 1
+                If vocal = frase(i) Then
+                    encontrado = encontrado + 1
+                End If
+            Next
+            Return encontrado
+
+        End If
+
+salir:
+    End Function
+
+
+
+End Class
