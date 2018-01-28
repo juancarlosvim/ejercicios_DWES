aed2739bce9904b90e531ee72559b97957408724
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2018-01-28 21:12:09 +0100

    eliminado proyecto

--- ProyectoTelefoodDWES/App_Code/detallePedido.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports Microsoft.VisualBasic
-
-Public Structure DetallePedido
-    Public Property idProducto As Int32
-    Public Property cantidad As Int32
-    Public Property pvp As Decimal
-    Public Property nombre As String
-End Structure
